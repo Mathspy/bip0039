7811f0aef5ad7fcde298aede0335dd6b6f7da817
Mathspy <mathspy257@gmail.com>
2017-11-30 20:53:23 +0200

    Function to39 submodule

--- src/to39.js
+++ src/to39.js
@@ -1,5 +1,18 @@
+const map39 = require("./map39")
+
 function to39(num) {
-	return num.toString(2);
+	return split11s(num.toString(2)).map(word => map39[parseInt(word, 2)]).reduce((sentence, word) => word + " " + sentence, "").trim();
+}
+
+function split11s(string) {
+	let arr = [[]]
+	for (var i = string.length - 1; i >= 0; i--) {
+		if (arr[0].length === 11) {
+			arr.unshift([]);
+		}
+		arr[0].unshift(string[i])
+	}
+	return arr.map(value => value.join(""))
 }
 
 module.exports = to39;
--- src/to39.test.js
+++ src/to39.test.js
@@ -2,6 +2,7 @@ const to39 = require("./to39")
 
 describe("to39", () => {
 	test("should turn number into bip39 words", () => {
-		expect(to39(40146749)).toBe("guess caught afraidgi")
+		expect(to39(40146749)).toBe("tray myself abuse")
+		expect(to39(28935854)).toBe("still toward absorb")
 	})
 })
